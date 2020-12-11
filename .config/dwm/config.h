f170f7aa27172961abfaab40becfeae648a58d6c
Melanich Hennadii <mhennadii@knu.ua>
2020-12-11 22:15:33 +0200

    add useless gaps patch

--- .config/dwm/config.h
+++ .config/dwm/config.h
@@ -1,10 +1,33 @@
+//static unsigned int borderpx  = 3;        /* border pixel of windows */
+//static unsigned int snap      = 32;       /* snap pixel */
+////static unsigned int gappih    = 20;       /* horiz inner gap between windows */
+////static unsigned int gappiv    = 10;       /* vert inner gap between windows */
+////static unsigned int gappoh    = 10;       /* horiz outer gap between windows and screen edge */
+////static unsigned int gappov    = 30;       /* vert outer gap between windows and screen edge */
+//static int swallowfloating    = 0;        /* 1 means swallow floating windows by default */
+//static int smartgaps          = 0;        /* 1 means no outer gap when there is only one window */
+//static int showbar            = 1;        /* 0 means no bar */
+//static int topbar             = 1;        /* 0 means bottom bar */
+//static const char *fonts[]          = { "fontawesome:size=13", "Iosevka Curly Medium:size=12"};
+//static const char dmenufont[]       = "Iosevka Curly Medium:size=13";
+//static char normbgcolor[]           = "#222222";
+//static char normbordercolor[]       = "#444444";
+//static char normfgcolor[]           = "#bbbbbb";
+//static char selfgcolor[]            = "#eeeeee";
+//static char selbordercolor[]        = "#770000";
+//static char selbgcolor[]            = "#005577";
+//static char *colors[][3] = {
+//       /*               fg           bg           border   */
+//       [SchemeNorm] = { normfgcolor, normbgcolor, normbordercolor },
+//       [SchemeSel]  = { selfgcolor,  selbgcolor,  selbordercolor  },
+//};
+
 static const unsigned int borderpx  = 0;        /* border pixel of Mod4Maskdows */
 static const unsigned int snap      = 0;//32;       /* snap pixel */
+static const unsigned int gappx     = 3;        /* gaps between windows */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int swallowfloating    = 1;        /* 1 means swallow floating windows by default */
 static const int topbar             = 1;        /* 0 means bottom bar */
-//static const char *fonts[]          = { "fontawesome:size=13", "Iosevka Medium:size=12"};
-//static const char dmenufont[]       = "Iosevka Medium:size=13";
 static const char *fonts[]          = { "fontawesome:size=13", "Iosevka Curly Medium:size=12"};
 static const char dmenufont[]       = "Iosevka Curly Medium:size=13";
 
@@ -59,7 +82,7 @@ static const Rule rules[] = {
 	{ "Zathura", 		            NULL,     	NULL,           1 << reader_tag,		    0,     		0,           	0,            -1 , 0},
 	{ "mpv",   		                NULL,      	NULL,		    1 << video_audio_tag,                 0,    		0,		        0,    -1 , 0},
 	{ "St",   		                NULL,       "cmus v2.8.0",	1 << video_audio_tag,                 0,    		0,		        0,    -1 , 0},
-	{ "St",   		                NULL,      	"ranger",   	1 << file_browser_tag,            0,    		0,		        0, 	      -1 , 0},
+	{ "St",   		                NULL,      	"ranger",   	1 << file_browser_tag,            0,    		1,		        0, 	      -1 , 0},
 	// make it stay on previous tag { "St",   		                NULL,      	NULL,   	1 << ,            0,    		0,		        0, 	      -1 , -1},
 	{ "Audacity",   		                NULL,       NULL,	1 << recording_tag,                 0,    		0,		        0,    -1 , 0},
 	{ "zoom",   		                NULL,       NULL,	1 << recording_tag,                 0,    		0,		        0,    -1 , 0},
